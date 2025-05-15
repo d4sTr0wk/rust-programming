9097bbcfafd15485c96026e3ad1e2b21378a57fe
maxgarci <maxgarci@localhost.localdomain>
2025-05-15 14:00:35 +0200

    rust: fibonacci code done

--- book_projects/3.common_concepts/fibonacci/src/main.rs
+++ book_projects/3.common_concepts/fibonacci/src/main.rs
@@ -1,3 +1,32 @@
+use std::io::{self, Write};
+
 fn main() {
-    print!("Introduce the index of the fibonacci number needed");
+    print!("Introduce the index of the fibonacci number needed: ");
+    std::io::stdout().flush().expect("Failed to flush stdout");
+
+    let index: usize = loop {
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        match input.trim().parse::<usize>() {
+            Ok(num) => break num,
+            Err(_) => println!("Invalid input. Please enter a valid number"),
+        }
+    };
+
+    let mut fn_0 = 0;
+    let mut fn_1 = 1;
+
+    if index == 0 {
+        fn_1 = 0;
+    } else if index != 1 {
+        for _ in 2..index {
+            let next = fn_0 + fn_1;
+            fn_0 = fn_1;
+            fn_1 = next;
+        }
+    }
+    println!("The fibonnaci number at index {index} is: {fn_1}");
 }
--- book_projects/3.common_concepts/fibonacci/target/debug/deps/fibonacci-2660ffa7b71483f0.d
+++
@@ -1,5 +0,0 @@
-/home/maxgarci/Documents/rust-programming/book_projects/3.common_concepts/fibonacci/target/debug/deps/libfibonacci-2660ffa7b71483f0.rmeta: src/main.rs
-
-/home/maxgarci/Documents/rust-programming/book_projects/3.common_concepts/fibonacci/target/debug/deps/fibonacci-2660ffa7b71483f0.d: src/main.rs
-
-src/main.rs:
